1fa9a7b7815ffd447badf8a9e688994b010e11bc
jopinta <tatinfa@yahoo.com>
2022-01-08 14:34:02 -0800

    configured to use the private key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,11 @@
 #Let’s practice using Puppet to make changes to our configuration file.
 
 file_line {'Turn off passwd auth':
-   path => '/etc/ssh/ssh_config'
-   line => 'PasswordAuthentication no' 
+   path => '/etc/ssh/ssh_config',
+   line => 'PasswordAuthentication no', 
+}
+
+file_line {'Declare identity file':
+   path => '/etc/ssh/ssh_config',
+   line => 'IdentityFile ~/.ssh/school',
 }
