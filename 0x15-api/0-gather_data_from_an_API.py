71417240d0700a6b07345a9566b42565de499d34
jopinta <tatinfa@yahoo.com>
2022-01-31 17:26:53 -0800

    test

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''use rest api'''
+
+
+import requests
+from sys import argv
+
+ID = int(argv[1])
+response = requests.get('https://jsonplaceholder.typcode.com/todos').json()
+user_list = requests.get('https://jsonplaceholder.typicode.com/users').json()
+count = 0
+todo[]
+total = 0
+
+for user in user_list:\
+    if user.get('id') == id:
+        name = user.get('name')
+        break
+
+    for todo in response:
+        if todo.get('userId') == id:
+            if todo.get('completed'):
+                todo.append(todo.get('title'))
+                count += 1
+            total += 1
+
+    print('Emplyee {} is done with tasks({}/{}):'.format(name,todo, total))
+    for task in tasks:
+        print('\t {}'.format(task)
+
+if __name__ == "__main__":
+            
+        
+
