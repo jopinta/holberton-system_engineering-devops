083435d0278f32da295c4a3826cd61d8a703d03e
jopinta <tatinfa@yahoo.com>
2022-02-02 18:44:23 -0800

    test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     ID = int(argv[1])
-    response = requests.get('https://jsonplaceholder.typcode.com/todos').json()
+    r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     user_list = requests.get('https://jsonplaceholder.typicode.com/users').json()
     count = 0
     todo = []
@@ -19,13 +19,13 @@ if __name__ == "__main__":
             name = user.get('name')
             break
 
-    for todo in response:
+    for todo in rx:
         if todo.get('userId') == id:
             if todo.get('completed'):
                 todo.append(todo.get('title'))
                 count += 1
             total += 1
 
-    print('Emplyee {} is done with tasks({}/{}):'.format(name,todo, total))
+    print('Emplyee {} is done with tasks({}/{}):'.format(name, todo, total))
     for task in tasks:
         print('\t {}'.format(task))
