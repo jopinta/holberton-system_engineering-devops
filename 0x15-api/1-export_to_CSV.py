342e9b68e1ee65e931f3d00c68fd845217e5aa96
jopinta <tatinfa@yahoo.com>
2022-02-15 08:34:56 -0800

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' como formmat'''
+
+if __name__ == "__main":
+    import csv
+    import requests
+    import sys
+
+    user_id = argv[1]
+    r1 = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                     format(user_id).json()
+    '''load = json.loads(r.text)
+    name + load.get("username")'''
+    r2 = requests.get('https://jsonplaceholder.typicode.com/todos/?userId={}'.
+                     format(user_id).json()
+    with open("{}.csv".format(user_id), newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOtE_ALL)
+        for task in r2:
+            writer.writerow([int(user_id),r1.get('username'),
+                             task.get('completed'),
+                             task.get('title')])
