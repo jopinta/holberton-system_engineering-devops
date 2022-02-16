f9e93c2d9c66d01dc6108ce61c44f6737e9229f4
jopinta <tatinfa@yahoo.com>
2022-02-15 18:51:26 -0800

    allow redirects F

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,8 @@ import requests
 def top_ten(subreddit):
     '''prints the titles of the first 10 hot posts '''
     r = requests.get("https://www.reddit.com/r/{}/hot.json".
-                     format(subreddit), headers={'User-Agent': 'Joa'})
+                     format(subreddit), headers={'User-Agent': 'Joa'},
+                     allow_redirects=False)
     if r.status_code == 200:
         subscriptions = r.json()["data"]["children"]
         for title in subscriptions:
