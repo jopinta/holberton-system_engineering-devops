d02ba2080f83b4f54b7878a07dadf5bfae83feca
jopinta <tatinfa@yahoo.com>
2022-02-16 18:03:03 -0800

    mini 90

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # use whatever Puppet resource
 exec { 'strace_is_your_friend':
-command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php,
+command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
 path => '/bin';
 }
