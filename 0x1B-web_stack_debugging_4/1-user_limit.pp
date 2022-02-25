9a1abd759719d0b06dfa139153fc6b920b9dba5f
jopinta <tatinfa@yahoo.com>
2022-02-25 11:03:46 -0800

    debbuging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Change the OS config
 exec { 'holbertons':
 command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" etc/security/limits.conf'
-provider => shell,
+provider => 'shell',
 }
 
 exec { 'holbertonh':
 command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" etc/security/limits.conf'
-privider => shell,
+privider => 'shell',
 }
