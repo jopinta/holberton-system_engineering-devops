30c57d08cc03f45696e4bfcaf222e857c90157b4
jopinta <tatinfa@yahoo.com>
2022-02-24 18:58:31 -0800

    for service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # under pressure and it turns out it’s not doing well
 exec {
-  command  => 'sed -i s/15/4069/ /etc/default/nginx; service nginx restart',
+  command  => 'sed -i s/15/4096/g /etc/default/nginx; service nginx restart',
   provider => shell,
 }
