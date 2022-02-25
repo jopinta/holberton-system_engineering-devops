761166c3e5f5a04b12ea1b7095fb1a2680cb8a87
jopinta <tatinfa@yahoo.com>
2022-02-25 09:50:27 -0800

    double colon

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# under pressure and it turns out it’s not doing well
-exec { 'Puppet-manifest'
+# it’s not doing well
+exec { 'Puppet-manifest':
   command  => 'sed -i s/15/4096/ /etc/default/nginx; sudo service nginx restart',
   provider => shell,
 }
