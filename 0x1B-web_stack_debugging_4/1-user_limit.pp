819ccbd86688a8cafbfd4efd107644165d1a3a95
jopinta <tatinfa@yahoo.com>
2022-02-25 09:44:14 -0800

    sed -i

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Change the OS config
 exec {
-command => s/holberton soft nofile 4/holberton soft nofile/g etc/security/limits.conf sed -i
+command => sed -i 's/holberton soft nofile 4/holberton soft nofile/g' etc/security/limits.conf sed -i
 provider => shell,
 }
 
 exce {
-command => s/holberton hard no file 5/holberton hard no file/g etc/security/limits.conf sed -i
+command => sed -i 's/holberton hard no file 5/holberton hard no file/g' etc/security/limits.conf
 privider => shell,
 }
