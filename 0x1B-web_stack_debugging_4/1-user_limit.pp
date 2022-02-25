69a18d37f4025dfa6bb736de6bab20e22b784b09
jopinta <tatinfa@yahoo.com>
2022-02-25 11:02:12 -0800

    debbuging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,7 +4,7 @@ command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" et
 provider => shell,
 }
 
-exce { 'holbertonh':
+exec { 'holbertonh':
 command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" etc/security/limits.conf'
 privider => shell,
 }
